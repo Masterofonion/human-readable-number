d9b8c6b676aabc9da232be31555512c1018e5029
Viacheslav Maltsev <79205849+Masterofonion@users.noreply.github.com>
2021-02-19 15:08:29 +0300

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function toReadable (number) {
-  
+    var zeroNntn = ['','one','two','three','four', 'five', 'six', 'seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    var dozens = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+    var zeros = ['hundred', 'thousand', 'million']
+    var strNum = '';
+    if (number == 0) {
+        return 'zero';
+    }
+    if (number >= 0 && number <= 19) {
+        return zeroNntn[number];
+    } else {
+        number = String(number).split('').map(Number);
+        
+        while (number.length > 0) {
+            if (number.length == 2) {
+                if (number[0] < 2) {
+                    strNum += ' ' + zeroNntn[Number(number[0]+ '' + number[1])];
+                    return strNum.trim();
+                } else {
+                    strNum += ' ' + dozens[number[0]-2] + ' ' + zeroNntn[number[1]];
+                    return strNum.trim();
+                }
+            } else { 
+                strNum += zeroNntn[number[0]] + ' ' + zeros[number.length -3];
+                number.shift();
+            }
+        }
+             
+        
+    }
+
 }
+
+
+
+
+
+
